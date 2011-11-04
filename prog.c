0204e9f8895d07291c776204a9caae7b62b12dd2
qtonneau@gmail.com <qtonneau@gmail.com@2b20b396-2896-7896-ba13-5336fe1519fc>
2011-11-04 16:16:23 +0000

    Correction partie 1 fonctionelle

--- prog.c
+++ prog.c
@@ -16,70 +16,75 @@ struct eth_frame * lire_trame() // procedure d'une trame
 
 struct tab_stat
 {
+	tab_stat *suivant;
 	char ad[13];
-	tab_stat* suivant;
 };
  
-int recherche(char* adr , tab_stat *stat ){
-printf("recherche\n");
+int recherche(char* adr , tab_stat* stat ){
+printf("recherche de %s dans la table..........................",adr);
 do
-{if ((strcmp(adr,stat->ad))==0)
-	{return 1;}
+{
+	if (strcmp(adr,stat->ad)==0)
+		{printf("trouve !\n");return 1;}
 	else {
-	stat=stat->suivant;
-	}	
-}while (stat != NULL);
+		stat=stat->suivant;
+	}
+}while (stat!= NULL);
+printf("non trouvé !\n");
 return 0;
 }
 
-tab_stat ajout (char* adr , tab_stat *stat ){
-printf("ajout\n");
-tab_stat nouveau;
-nouveau.suivant=stat; //bordel
-strcpy(nouveau.ad,adr);
-return nouveau;
+tab_stat* ajout (char* adr , tab_stat* stat ){
+	printf("ajout de %s dans la table........................",adr);
+	tab_stat* nouveau=(tab_stat*)malloc (sizeof (tab_stat));
+	strcpy(nouveau->ad,adr);
+	nouveau->suivant=stat;
+	printf("ok !\n");
+	return nouveau;
 }
 
-int affiche(tab_stat *stat){
+int affiche(tab_stat* stat){
+printf("Adresses détectées sur le réseau : \n");
+int cpt;
+cpt=4;
 do
-{printf("%s" , &stat->ad);
- stat=stat->suivant;	
-}while (stat != NULL);
+{printf("%s\n" , stat->ad);
+ stat=stat->suivant;
+ cpt--;	
+}while (stat != NULL && cpt!=0);
 return 1;
 }
 
 int main ()
 {
 	struct eth_frame *trame;
-	char *ad = "000000000000";
-	char *ad_source = "000000000000";
-	char *ad_dest ="000000000000";
-	int i=100;
-	tab_stat stat;
-	stat.suivant = NULL;
-	char *chaine ="ffffffffffff";
-	strcpy(stat.ad,chaine);
-	affiche(&stat);
+	int i=4;			//LIMITE DE BOUCLE (TRAVAIL LOCAL)
+	tab_stat* stat=(tab_stat*)malloc (sizeof (tab_stat));
+	//affiche(stat);		//AFFICHE UN TABLEAU VIDE
 	while(i>0)
-	{	printf("testdeb\n");
+	{	
 		trame = lire_trame();
-		ad_dest = char_to_charhexa(trame -> adr_dest,6);
-		ad_source = char_to_charhexa(trame -> adr_send,6);
-		printf("test recherche\n");
-		if (recherche(ad_dest, &stat))
-		{
-		stat = ajout(ad_dest, &stat);
-		}
-		if (recherche(ad_source, &stat))
-		{stat = ajout(ad_source, &stat);
-		}
+		//on imprime les informations obtenues
 		printf("Type : %s \n", char_to_charhexa(trame -> type,2));
 		printf("Adresse dest : %s \n", char_to_charhexa(trame -> adr_dest,6));
 		printf("Adresse source : %s \n", char_to_charhexa(trame -> adr_send,6));
-		i--;
-		printf("testfin\n");
+
+		//on recherche et ajoute les deux adresses dans la table "stat"
+		printf("###########\n");
+		if (!recherche(char_to_charhexa(trame -> adr_dest,6), stat))
+		{
+			stat = ajout(char_to_charhexa(trame -> adr_dest,6), stat);
+		}
+		if (!recherche(char_to_charhexa(trame -> adr_send,6), stat))
+		{
+			stat = ajout(char_to_charhexa(trame -> adr_send,6), stat);
+		}
+		printf("###########\n\n\n");
+
+
+		i--;//Supprimer en cas de non-limite à la boucle
 	}
-	affiche(&stat);
+	affiche(stat);			//AFFICHE LE RESULTAT
 	return 0;
 }
 
