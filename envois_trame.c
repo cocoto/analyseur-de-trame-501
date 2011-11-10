93e7ca2a6b30967558606dddac40592294908440
qtonneau@gmail.com <qtonneau@gmail.com@2b20b396-2896-7896-ba13-5336fe1519fc>
2011-11-10 17:47:22 +0000

    introduction à l'envois des trames

---
+++ envois_trame.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "eth_lib.h"
+void envois_trame(char *source, char *dest, char *message)
+{	
+	//Génération de la trame en question
+	char *type="9000";
+	struct eth_frame *latrame;
+	int taille=make_ping_request(dest,source,type,message,latrame);
+	char *tramehexa=char_to_charhexa((unsigned char*)&latrame,taille);
+	char commande[500];
+	strcpy(commande,"./write_eth_frame ");
+	strcat(commande,tramehexa);
+	printf("%s\n",commande);
+	system(commande);
+}
+int main ()
+{
+	envois_trame("00:12:3f:3c:5e:6e","00:12:3f:3c:5e:da","coucou");
+	return 0;
+}
