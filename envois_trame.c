2ffc595e02949b7a159484fc47d98431b35d0c27
qtonneau@gmail.com <qtonneau@gmail.com@2b20b396-2896-7896-ba13-5336fe1519fc>
2011-11-24 12:02:09 +0000

    ca marche cd analyseur-de-trame-501/

--- envois_trame.c
+++ envois_trame.c
@@ -8,21 +8,21 @@ void envois_trame(char *source, char *dest, char *message)
 {	
 	//Génération de la trame en question
 	char *type="9000";
-	struct eth_frame *latrame=malloc(sizeof(struct eth_frame));
-	int taille=make_ping_request(dest,source,type,message,latrame);
+	struct eth_frame latrame;
+	int taille=make_ping_request(dest,source,type,message,&latrame);
 	char *tramehexa=char_to_charhexa((unsigned char*)&latrame,taille);
 	char commande[500];
 	strcpy(commande,"./write_eth_frame ");
 	strcat(commande,tramehexa);
-	printf("%s\n",commande);
+	//printf("%s\n",commande);
 	system(commande);
 }
 
 /* ALGORITHME DE TEST */
-/*
-int main ()
+
+/*int main ()
 {
-	envois_trame("00:12:3f:3c:5e:6e","00:12:3f:3c:5e:da","coucou");
+	envois_trame("00:12:3f:3d:96:57","ff:ff:ff:ff:ff:ff","Bonjour de Quentin & Adrien");
 	return 0;
-}
-*/
+}*/
+
