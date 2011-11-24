512d79d79c7e6dd5a794b0e0c28165f7b5b5004d
qtonneau@gmail.com <qtonneau@gmail.com@2b20b396-2896-7896-ba13-5336fe1519fc>
2011-11-24 12:02:09 +0000

    ca marche cd analyseur-de-trame-501/

--- trunk/projet.c
+++ trunk/projet.c
@@ -3,6 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 //Fonction de conversion d'une adress de type "00123f3c5e6e" en "00:12:3f:3c:5e:6e"
+int est_dans(char * chaine, char car)
+{
+	int i;
+	for(i=0;i<strlen(chaine);i++)
+	{
+		if(chaine[i]==car)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
 void convaddr(char* adresse, char* adr_dest)
 {
 	sprintf(adr_dest,"%c%c:%c%c:%c%c:%c%c:%c%c:%c%c",
@@ -38,23 +50,34 @@ int main (int argc, char *argv[])
 {
 	char* adresse=argv[1];	//Notre adresse passée en paramètre
 	char * message=" : recu par Quentin & Adrien";
-  char * adr_hexa=malloc(sizeof(char)*18);
+  	char * adr_hexa=malloc(sizeof(char)*18);
+  	int i=0;
 	while(1)
-	{
+	{	
+		if(i<1000)
+		{
+			envois_trame(adresse,"ff:ff:ff:ff:ff:ff","Bonjour de Quentin & Adrien");
+			i++;
+		}
 		/*==============ENVOIS EN BOUCLE DE BONJOUR !=====================*/
-		envois_trame(adresse,"ff:ff:ff:ff:ff:ff","Bonjour de Quentin & Adrien");
+		
+		//envois_trame("00:12:3f:3d:96:57","ff:ff:ff:ff:ff:ff","Bonjour de Quentin & Adrien");
 		
 		
 		/*==============REPONSE AUX BONJOURS=========================*/
 		struct eth_frame *trame = lire_trame();
-    convaddr(char_to_charhexa(trame-> adr_dest,6),adr_hexa);
+    		convaddr(char_to_charhexa(trame-> adr_dest,6),adr_hexa);
 		if((strcmp(adr_hexa,adresse)==0 || strcmp(adr_hexa,"ff:ff:ff:ff:ff:ff")==0) && strcmp(char_to_charhexa(trame -> type,2),"9000")==0)
 		//Si nous sommes le destinataire et type=9000
 		{
-			if(strstr(message,"Bonjour de")!=NULL && strstr(message,"reçu par")==NULL)
+			
+			if(est_dans(trame->data,':')!=1)
 			//Si le message contient "Bonjour de", mais ne contient pas "reçu par" (évite les boucles)
 			{
-				envois_trame(adresse,convaddr(char_to_charhexa(trame-> adr_send,6)),strcat(char_to_charhexa(trame->data,strlen(trame->data)),message));
+				printf("Message : %s \n",trame->data);
+				char * adr_rec_hexa=malloc(sizeof(char)*18);
+				convaddr(char_to_charhexa(trame-> adr_send,6),adr_rec_hexa);
+				envois_trame(adresse,adr_rec_hexa,strcat(trame->data,message));
 			}
 		}
 	}
