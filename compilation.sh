bf3e1aafa6598ba9cb1a566eeb64305a700ac471
qtonneau@gmail.com <qtonneau@gmail.com@2b20b396-2896-7896-ba13-5336fe1519fc>
2011-11-28 12:32:23 +0000

    structuration du projet

--- compilation.sh
+++ compilation.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+mkdir bin
 gcc eth_lib.c -c
-gcc write_eth_frame.c eth_lib.o -o write_eth_frame
-gcc envoyer_trame.c eth_lib.o fonctions.c -o envois
-gcc recevoir_trame.c eth_lib.o fonctions.c -o recevoir
-gcc reponse_auto.c eth_lib.o fonctions.c -o reponse
-
+gcc write_eth_frame.c eth_lib.o -o bin/write_eth_frame
+gcc envoyer_trame.c eth_lib.o fonctions.c -o bin/envois
+gcc recevoir_trame.c eth_lib.o fonctions.c -o bin/recevoir
+gcc reponse_auto.c eth_lib.o fonctions.c -o bin/reponse
+rm eth_lib.o
