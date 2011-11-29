6f7b2cbe592c731751df851fff14555ec66a1927
qtonneau@gmail.com <qtonneau@gmail.com@2b20b396-2896-7896-ba13-5336fe1519fc>
2011-11-29 15:10:29 +0000

    maj script compilation + debut rapport

--- compilation.sh
+++ compilation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir bin
-gcc eth_lib.c -c
-gcc write_eth_frame.c eth_lib.o -o bin/write_eth_frame
-gcc sniff.c eth_lib.o -o bin/sniff
-gcc envoyer_trame.c eth_lib.o fonctions.c -o bin/envois
-gcc recevoir_trame.c eth_lib.o fonctions.c -o bin/recevoir
-gcc reponse_auto.c eth_lib.o fonctions.c -o bin/reponse
+gcc src/eth_lib.c -c
+gcc src/write_eth_frame.c eth_lib.o -o bin/write_eth_frame
+gcc src/sniff.c eth_lib.o -o bin/sniff
+gcc src/envoyer_trame.c eth_lib.o src/fonctions.c -o bin/envois
+gcc src/recevoir_trame.c eth_lib.o src/fonctions.c -o bin/recevoir
+gcc src/reponse_auto.c eth_lib.o src/fonctions.c -o bin/reponse
 rm eth_lib.o
